d6e3478c19aa9262d837dddee3adeebac7fecd86
perompak_kode <iqbaltech29@gmail.com>
2023-04-12 00:30:39 +0700

    Routing dan Pemberian atribut {{ Dasar Routing }}

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    /* Route berfungsi untuk menentukan kemana file akan dituju untuk sekarang route digunakan untuk menampilkan (Mencetak Tulisan) namun pada
+        fungsinya untuk menentukan tampilan mana yang akan ditampilkan ketika pengguna melakukan interaksi terhadap web
+    */
+    // Return mengembalikan value (Nilai) pada bentuk umum bahasa pemrograman
+    return ('Hallo Dunia aku Iqbal Maulana Bersiap Untuk merasuk ke dalam Laravel 8');
+});
+
+Route::get('/go', function () {
+    /*
+    Pada return view di bawah terdapat routing yang mengarah pada file welcome.blade.php dan terdapat atribut array asosiatif yang membawa
+    nilai yang nanti dapat di panggil atau di gunakan di views
+    */
+    return view('welcome',
+     [
+        'namaTop'=>'IqLRV8',
+        'nama' => 'Mochamad Iqbal Maulana',
+        'nim'=>'E41212178',
+        'kampus'=>'POLIJE Bondowoso',
+        'asal'=>'Lumajang'
+
+    ]);
 });
