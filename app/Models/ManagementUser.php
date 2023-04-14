9062b45970ca0f13ee57e3091960246dfa087b2c
perompak_kode <iqbaltech29@gmail.com>
2023-04-14 17:30:59 +0700

    Minggu 3/4 ACARA 12. Membuat Model View Controller Pada Laravel (4)

--- app/Models/ManagementUser.php
+++ app/Models/ManagementUser.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// Ini adalah file model berfungsi untuk menentukan kolom apa saja yang dapat di isi atau di input ke dalam database.
 class ManagementUser extends Model
 {
 
